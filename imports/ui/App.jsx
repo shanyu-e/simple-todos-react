af4d91bee6d73114c8e1014878302c44d5cef550
shanyu-e <554816284@qq.com>
2016-04-07 11:23:08 +0800

    不记得了

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,18 +1,22 @@
+import React, { Component } from 'react';
+
+import Task from './Task.jsx';
+
 // App component - represents the whole app
-App = React.createClass({
+export default class App extends Component {
     getTasks() {
         return [
-            { _id: 1, text: "This is task 1" },
-            { _id: 2, text: "This is task 2" },
-            { _id: 3, text: "This is task 3" }
+            { _id: 1, text: 'This is task 1' },
+            { _id: 2, text: 'This is task 2' },
+            { _id: 3, text: 'This is task 3' },
         ];
-    },
+    }
 
     renderTasks() {
-        return this.getTasks().map((task) => {
-            return <Task key={task._id} task={task} />;
-        });
-    },
+        return this.getTasks().map((task) => (
+            <Task key={task._id} task={task} />
+        ));
+    }
 
     render() {
         return (
@@ -27,4 +31,4 @@ App = React.createClass({
             </div>
         );
     }
-});
+}
